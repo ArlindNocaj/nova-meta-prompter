e6c1fb43b9ce2e0deec463cf022e505fe703c551
Arlind Nocaj <arlind.nocaj@gmail.com>
2025-10-08 18:55:52 +0200

    increment verison

--- nova_meta_prompter/__init__.py
+++ nova_meta_prompter/__init__.py
@@ -6,5 +6,5 @@ A simple library to transform prompts to align with Amazon Nova guidelines.
 
 from .transform import transform_prompt
 
-__version__ = "0.1.1"
+__version__ = "0.1.3"
 __all__ = ["transform_prompt"]
